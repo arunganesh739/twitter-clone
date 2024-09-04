451fad2eba3740ce2118c3cf9885438fa5def3f5
arunganesh <arunganesh739@gmail.com>
2024-09-05 00:40:26 +0530

    3- delete post added

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -37,7 +37,7 @@ const LoginPage = () => {
 		},
 		onSuccess: () => {
 			toast.success("Login successful!");
-			queryClient.invalidateQueries({ queryKey: ["authUser"] });
+			queryClient.invalidateQueries({ queryKey: ["authUser"] }); 
 		},
 		onError: (error) => {
 			toast.error(error.message || "Login failed");
