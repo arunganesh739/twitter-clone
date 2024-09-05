30f0d763f78a90d44c73854be7c4d2e0ba18e81c
arunganesh <arunganesh739@gmail.com>
2024-09-06 00:26:40 +0530

    3- useFollow hook added

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,35 +1,38 @@
 import { Link } from "react-router-dom";
-import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import { useQuery } from "@tanstack/react-query";
 
-// Your component code continues here...
+import useFollow from "../../hooks/useFollow";
+
+import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
+import LoadingSpinner from "./LoadingSpinner";
 
 const RightPanel = () => {
-	const {data:suggestedUsers, isLoading} = useQuery({
-		queryKey:["suggestedUsers"],
+	const { data: suggestedUsers, isLoading } = useQuery({
+		queryKey: ["suggestedUsers"],
 		queryFn: async () => {
 			try {
-				const res = await fetch ("api/users/suggested")
+				const res = await fetch("/api/users/suggested");
 				const data = await res.json();
-				if(!res.ok) {
-					throw new Error(data.message || "Something Went Wrong!");
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong!");
 				}
 				return data;
-				
 			} catch (error) {
-			  throw new Error(error.message);
-				
+				throw new Error(error.message);
 			}
 		},
 	});
-	if(suggestedUsers?.length === 0) return<div className='md:W-64 w-0'></div>
+
+	const { follow, isPending } = useFollow();
+
+	if (suggestedUsers?.length === 0) return <div className='md:w-64 w-0'></div>;
 
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
 			<div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
 				<p className='font-bold'>Who to follow</p>
 				<div className='flex flex-col gap-4'>
-					item
+					{/* item */}
 					{isLoading && (
 						<>
 							<RightPanelSkeleton />
@@ -37,7 +40,7 @@ const RightPanel = () => {
 							<RightPanelSkeleton />
 							<RightPanelSkeleton />
 						</>
-					)}    
+					)}
 					{!isLoading &&
 						suggestedUsers?.map((user) => (
 							<Link
@@ -61,9 +64,12 @@ const RightPanel = () => {
 								<div>
 									<button
 										className='btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm'
-										onClick={(e) => e.preventDefault()}
+										onClick={(e) => {
+											e.preventDefault();
+											follow(user._id);
+										}}
 									>
-										Follow
+										{isPending ? <LoadingSpinner size='sm' /> : "Follow"}
 									</button>
 								</div>
 							</Link>
